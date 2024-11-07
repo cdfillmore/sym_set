8196a06554c4e54b97b0b501ffb1aa9a82bb9d75
christopher.fillmore@ist.ac.at <christopher.fillmore@ist.ac.at>
2024-11-06 23:58:56 -0500

    updated file

--- sym_set.py
+++ sym_set.py
@@ -3,6 +3,65 @@ import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi
 from scipy.spatial.distance import cdist
 import gudhi
+import itertools as it
+import os
+import collections
+import colorsys
+
+def read_obj(path):
+    pts = []
+    simps = []
+    with open(path,"r") as f:
+        for line in f:
+            if line[0] == 'v':
+                pts += [[float(i) for i in line.strip('\n').strip(' ').split(' ')[1:4]]]
+            elif line[0] == 'f':
+                simps += [[ int(i)-1 for i in line.strip('\n').strip(' ').split(' ')[1:4]]]
+    return np.array(pts), np.array(simps)
+
+def read_obj_normals(path):
+    pts = []
+    v_norms = []
+    simps = []
+    with open(path,"r") as f:
+        for line in f:
+            if line[:2] == 'v ':
+                pts += [[float(i) for i in line.strip('\n').strip(' ').split(' ')[1:4]]]
+            elif line[:2] == 'vn':
+                norm = [float(i) for i in line.strip('\n').strip(' ').split(' ')[1:4]]
+                v_norms += [ norm/np.linalg.norm(norm) ]
+            elif line[:2] == 'f ':
+                simps += [[ int(i.split('/')[0])-1 for i in line.strip('\n').strip(' ').split(' ')[1:4]]]
+    return np.array(pts), np.array(v_norms), np.array(simps)
+
+
+def read_obj_curve(path):
+	pts = []
+	simps = []
+	with open(path,"r") as f:
+		for line in f:
+			if line[0] == 'v':
+				pts += [[float(i) for i in line.strip('\n').strip(' ').split(' ')[1:4]]]
+			elif line[0] == 'l':
+				simps += [[ int(i)-1 for i in line.strip('\n').strip(' ').split(' ')[1:3]]]
+	pts = np.array(pts)
+	graph = {i:[] for i in range(len(simps))}
+	for i,j in simps:
+		graph[i].append(j)
+		graph[j].append(i)
+	out_indices = [0, graph[0][0]]
+	while out_indices[-1] != 0:
+		current = out_indices[-1]
+		old = out_indices[-2]
+		new = list(set(graph[current]).difference([old]))[0]
+		out_indices.append(new)
+	return pts[out_indices]
+
+def make_curve2d(curve, noise=None):
+    if noise:
+        return np.array([curve.T[0], curve.T[2]]).T + noise*np.random.rand(len(curve),2)
+    else:
+        return np.array([curve.T[0], curve.T[2]]).T 
 
 def distance(pair):
     """
@@ -75,11 +134,51 @@ def circle_through_three_points(p1, p2, p3):
     
     return center, radius
 
-def evolute(pts, plot=True):
+#probably not needed
+def sphere_through_four_points(p1, p2, p3, p4):
+    """
+    Compute the center and radius of a sphere passing through four points in 3D space.
+    
+    :param p1: First point (x1, y1, z1)
+    :param p2: Second point (x2, y2, z2) 
+    :param p3: Third point (x3, y3, z3)
+    :param p4: Fourth point (x4, y4, z4)
+    :return: Tuple (center_x, center_y, center_z, radius)
+    """
+    # Convert input points to numpy arrays
+    p1, p2, p3, p4 = np.array(p1), np.array(p2), np.array(p3), np.array(p4)
+    
+    # Set up the system of linear equations
+    # For a sphere, (x - c)^2 + (y - c_y)^2 + (z - c_z)^2 = r^2 for each point
+    # Subtracting equations eliminates r^2 term
+    A = np.array([
+        [2*(p2-p1)[0], 2*(p2-p1)[1], 2*(p2-p1)[2]],
+        [2*(p3-p1)[0], 2*(p3-p1)[1], 2*(p3-p1)[2]],
+        [2*(p4-p1)[0], 2*(p4-p1)[1], 2*(p4-p1)[2]]
+    ])
+    
+    b = np.array([
+        np.dot(p2,p2) - np.dot(p1,p1),
+        np.dot(p3,p3) - np.dot(p1,p1),
+        np.dot(p4,p4) - np.dot(p1,p1)
+    ])
+    
+    # Solve for center
+    center = np.linalg.solve(A, b)
+    
+    # Compute radius
+    radius = np.sqrt(np.sum((p1 - center)**2))
+    
+    return center, radius
+
+
+def evolute(pts, thresh, plot=True):
     centres = []
     for i in range(len(pts)):
         triple = pts[np.r_[(i-3):i]]
-        centres += [circle_through_three_points(*triple)[0]]
+        circ = circle_through_three_points(*triple)
+        if circ[1] < thresh:
+             centres += [circ[0]]
     centres = np.array(centres)
     if plot==True:
         plt.figure(figsize=(10, 8))
@@ -94,6 +193,61 @@ def evolute(pts, plot=True):
         plt.legend(['Evolute', 'Original Curve'])
         plt.show()
     return centres
+
+# incomplete
+# Estimating Curvatures and Their Derivatives on Triangle Meshes
+# Szymon Rusinkiewicz
+def evolute_3d(pts, v_norms, tris, thresh):
+    #calculate coordinate sys for each vertex (u_p, v_p)
+    rand_vect = np.random.rand(1,3)
+    v_coords = []
+    for p, n_p in zip(pts, v_norms):
+        u_p = np.cross(n_p, rand_vect)
+        v_p = np.cross(n_p, u_p)
+        v_coords += [[u_p, v_p]]
+    for tri in tris:
+        n_f = v_norms[tri].sum(axis=0)
+        u_f = np.cross(n_f, rand_vect)
+        v_f = np.cross(n_f, u_f)
+        edge_vectors = []
+        norm_deltas = []
+        for (v1, v2) in list(it.combinations(tri, 2)):
+            edge_vectors += [pts[v1] - pts[v2]]
+            norm_deltas += [ v_norms[v1] - v_norms[v2]]
+        x1 = np.dot(edge_vectors[0], u_f.T)[0]
+        x2 = np.dot(edge_vectors[0], v_f.T)[0]
+        y1 = np.dot(edge_vectors[1], u_f.T)[0]
+        y2 = np.dot(edge_vectors[1], v_f.T)[0]
+        z1 = np.dot(edge_vectors[2], u_f.T)[0]
+        z2 = np.dot(edge_vectors[2], v_f.T)[0]
+
+        a1 = np.dot(norm_deltas[0], u_f.T)[0]
+        a2 = np.dot(norm_deltas[0], v_f.T)[0]
+        b1 = np.dot(norm_deltas[0], u_f.T)[0]
+        b2 = np.dot(norm_deltas[0], v_f.T)[0]
+        c1 = np.dot(norm_deltas[0], u_f.T)[0]
+        c2 = np.dot(norm_deltas[0], v_f.T)[0]
+
+        A = np.array(
+            [[x1,x2,0],
+            [0,x1,x2],
+            [y1,y2,0],
+            [0,y1,y2],
+            [z1,z2,0],
+            [0,z1,z2]])
+        b = np.array([a1,a2,b1,b2,c1,c2]).T
+        x = np.linalg.lstsq(A,b)[0]
+        fund_form2_f = np.array(
+            [[x[0], x[1]],
+            [x[1], x[2]]])
+        #break
+        for vert in tri:
+            u_p, v_p = v_coords[vert][0], v_coords[vert][1]
+            e_p = np.matmul(np.matmul(np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]), fund_form2_f), np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]).reshape([2,1]))[0]
+            #double check u, v if something goes wrong
+            f_p = np.matmul(np.matmul(np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]), fund_form2_f), np.array([np.dot(v_p, u_f.T)[0][0], np.dot(v_p, v_f.T)[0][0]]).reshape([2,1]))[0]
+            g_p = np.matmul(np.matmul(np.array([np.dot(v_p, u_f.T)[0][0], np.dot(v_p, v_f.T)[0][0]]), fund_form2_f), np.array([np.dot(v_p, u_f.T)[0][0], np.dot(v_p, v_f.T)[0][0]]).reshape([2,1]))[0]
+        break
 
 def compute_2d_lambda_medial_axis(points, lambda_value, farthest=False):
     # Compute Voronoi diagram
@@ -125,15 +279,43 @@ def compute_2d_lambda_medial_axis(points, lambda_value, farthest=False):
     
     return vertices, lambda_ma_vertices, lambda_ma_edges
 
-def plot_medial_evolute(pts, lambda_value, pt, outfile):
-    vertices0, lambda_ma_vertices0, lambda_ma_edges0 = compute_2d_lambda_medial_axis(pts, lambda_value, False)
-    vertices1, lambda_ma_vertices1, lambda_ma_edges1 = compute_2d_lambda_medial_axis(pts, lambda_value, True)
-    evo = evolute(pts, plot=False)
+# prints the evolute and medial axes for input of multiple curves
+# first curve goes to pts additional curves can be added to args
+def plot_medial_evolute(pts, lambda_value, pt, outfile=None, *args):
+    extra_curves = [ar for ar in args]
+    med_points = pts.tolist()
+    if len(extra_curves) > 0:
+        for curve in extra_curves:
+            med_points +=  curve.tolist()
+    med_points = np.array(med_points)
+    frac = .2
+    vertices0, lambda_ma_vertices0, lambda_ma_edges0 = compute_2d_lambda_medial_axis(med_points, lambda_value, False)
+    vertices1, lambda_ma_vertices1, lambda_ma_edges1 = compute_2d_lambda_medial_axis(med_points, lambda_value, True)
+    evos = []
+    if len(extra_curves) > 0:
+        evos = [evolute(curve, np.inf*lambda_value, plot=False) for curve in extra_curves]
+    evos += [evolute(pts, np.inf*lambda_value, plot=False)]
     
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(35, 15))
     
-    ax1.plot(evo.T[0], evo.T[1],'r', label='Evolute')
-    ax1.plot(pts.T[0], pts.T[1],'k', label='Original Curve')
+    for evo in evos:
+        colour_range = [colorsys.hsv_to_rgb(x,1,1) for x in np.linspace(0,1,len(evo))]
+        for i in range(len(evo)):
+            ax1.plot(evo.T[0][np.r_[(i-2):i]], evo.T[1][np.r_[(i-2):i]], color=colour_range[i]) #, label='Evolute')
+    '''
+    jump_indices = [i for i in range(len(evo)) if distance((evo[i], evo[(i+1)%len(evo)])) > 5]
+    k=0
+    for j in jump_indices:
+        ax1.plot(evo[k:(j)].T[0], evo[k:(j)].T[1], color='r')
+        k=j
+    '''
+    colour_range = [colorsys.hsv_to_rgb(x,1,1) for x in np.linspace(0,1,len(pts))]
+    for i in range(len(pts)):
+        ax1.plot(pts.T[0][np.r_[(i-2):i]], pts.T[1][np.r_[(i-2):i]], color=colour_range[i])#, label='Original Curve')
+    for curve in extra_curves:
+        colour_range = [colorsys.hsv_to_rgb(x,1,1) for x in np.linspace(0,1,len(curve))]
+        for i in range(len(curve)):
+            ax1.plot(curve.T[0][np.r_[(i-2):i]], curve.T[1][np.r_[(i-2):i]], color=colour_range[i])#, label='Original Curve')
     ax1.scatter([pt[0]], [pt[1]], color='k', label='Filtration Point')
     # Plot lambda-MA vertices
     #plt.scatter(lambda_ma_vertices[:, 0], lambda_ma_vertices[:, 1], c='g', label='λ-MA Vertices')
@@ -160,22 +342,54 @@ def plot_medial_evolute(pts, lambda_value, pt, outfile):
     ax1.axis('equal')
     ax1.grid(True)
     ax1.legend()
+    ax1.set_xlim([pts.T[0].min()*(1 - frac*np.sign(pts.T[0].min())), pts.T[0].max()*(1 + frac*np.sign(pts.T[0].max()))])
+    ax1.set_ylim([pts.T[1].min()*(1 - frac*np.sign(pts.T[1].min())), pts.T[1].max()*(1 + frac*np.sign(pts.T[1].max()))])
 
     cplx = gudhi.SimplexTree()
     cplx.set_dimension(2)
     for i in range(len(pts)):
         cplx.insert([i], filtration=np.linalg.norm(pts[i] - np.array(pt)))
         cplx.insert([i, (i+1)%len(pts)], filtration=max([np.linalg.norm(pts[i] - np.array(pt)), np.linalg.norm(pts[(i+1)%len(pts)] - np.array(pt))]))
-    cplx.compute_persistence(persistence_dim_max=2)
-    gudhi.plot_persistence_diagram(cplx.persistence(), axes=ax2)
-    fig.savefig(outfile)
+    counter = len(pts)
+    for curve in extra_curves:
+        for i in range(len(curve)):
+            cplx.insert([i + counter], filtration=np.linalg.norm(curve[i] - np.array(pt)))
+            cplx.insert([i + counter, (i+1)%len(curve) + counter], filtration=max([np.linalg.norm(curve[i] - np.array(pt)), np.linalg.norm(curve[(i+1)%len(curve)] - np.array(pt))]))
+        counter += len(curve)
+    cplx.extend_filtration()
+    #cplx.compute_persistence(persistence_dim_max=2)
+    dgms = cplx.extended_persistence()#persistence_dim_max=2)
+    flat_dgms = [item for sublist in dgms for item in sublist]
+    diag0 = np.array([ i[1] for i in flat_dgms if i[0]==0])
+    diag1 = np.array([ i[1] for i in flat_dgms if i[0]==1])
+    max_births, max_deaths = max([ i[1][0] for i in flat_dgms]), max([ i[1][1] for i in flat_dgms])
+    max_diag = max(max_births, max_deaths)
+
+    #gudhi.plot_persistence_diagram(cplx.persistence(), axes=ax2)
+    ax2.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
+    ax2.scatter(diag0.T[0], diag0.T[1], color='b', label='0th Diagram')
+    ax2.scatter(diag1.T[0], diag1.T[1], color='r', label='1st Diagram')
+    ax2.legend()
+    ax2.set_title('Persistence Diagram')
+    ax2.set_xlabel('Birth')
+    ax2.set_ylabel('Death')
+    ax2.axis('equal')
+    ax2.grid(True)
+    ax2.legend()
+    #ax2.set_xlim([-0.1, 15])
+    #ax2.set_ylim([-0.1, 15])
+    if outfile==None:
+         plt.show()
+    else:
+        fig.savefig(outfile)
     plt.close(fig)  # Close the figure to free up memory
-    pass
+    return dgms
 
 
 
-lambda_val = .1
+lambda_val = .4
 n = 1000
+frames = 150
 
 egg_x = lambda t: ((36 - np.sin(t)*np.sin(t))**(1 / 2) + np.cos(t))*np.cos(t)
 egg_y = lambda t: 4*np.sin(t)
@@ -191,16 +405,51 @@ pts = np.array(pts_egg.tolist() + pts_ellipse.tolist())
 
 
 # make animation
-for i,x in enumerate(np.linspace(8, 4, 300)):
+for i,x in enumerate(np.linspace(8, 4, frames)):
     print(i)
-    plot_medial_evolute(pts_egg, lambda_val, [3, x], './anim/frame_{}.png'.format(str(i).zfill(4)))
+    dgms = plot_medial_evolute(pts_egg, lambda_val, [3, x], './anim/frame_{}.png'.format(str(i).zfill(4)))
+for i,x in enumerate(np.linspace(-1, 3, frames)):
+    print(frames + i)
+    dgms = plot_medial_evolute(pts_egg, lambda_val, [x, 8], './anim/frame_{}.png'.format(str(frames + i).zfill(4)))
+os.sys("ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence.mp4")
 
 
 
+circ_x = lambda t: np.cos(t)
+circ_y = lambda t: np.sin(t)
+pts_circ = plot_parametric_2d(circ_x, circ_y, [-np.pi, np.pi], num_points=frames, title="2D Parametric Plot")[:-1]
+pts_circ += np.array([1.1, 1.5])
+# make animation
+dgms_all = []
+for i,x in enumerate(pts_circ):
+    print(i)
+    dgms_all += [plot_medial_evolute(ellipse_big, 0.1, x, './anim3/frame_{}.png'.format(str(i).zfill(4)), ellipse_small)]
+#os.system("ffmpeg -framerate 30 -pattern_type glob -i './anim3/*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence.mp4")
 
 
+flat_dgms_all = [[item for sublist in dgm for item in sublist] for dgm in dgms_all]
+max_births, max_deaths = max([max([ i[1][0] for i in flat_dgm]) for flat_dgm in flat_dgms_all]), max([max([ i[1][1] for i in flat_dgm]) for flat_dgm in flat_dgms_all])
+max_diag = max(max_births, max_deaths)
+flat_dgms_all = [[item for sublist in flat_dgms_all for item in sublist] for dgm in flat_dgms_all]
+
+flatter_dgms_all = [item for sublist in flat_dgms_all for item in sublist]
+diag0 = np.array([ i[1] for i in flatter_dgms_all if i[0]==0])
+diag1 = np.array([ i[1] for i in flatter_dgms_all if i[0]==1])
 
 
+plt.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
+plt.scatter(diag0.T[0], diag0.T[1], color='b', label='0th Diagram')
+plt.scatter(diag1.T[0], diag1.T[1], color='r', label='1st Diagram')
+plt.legend()
+#plt.set_title('Persistence Diagram')
+#plt.set_xlabel('Birth')
+#plt.set_ylabel('Death')
+plt.axis('equal')
+plt.grid(True)
+plt.legend()
+#plt.set_xlim([-0.1, max_diag])
+#plt.set_ylim([-0.1, max_diag])
+plt.show()
 
 
 
