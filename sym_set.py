615c048cb5fd02e57967dfc2ee6ed0785d1b5578
christopher.fillmore@ist.ac.at <christopher.fillmore@ist.ac.at>
2024-11-15 11:49:52 -0500

    update final friday notre dame

--- sym_set.py
+++ sym_set.py
@@ -1,12 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import Voronoi
+from scipy.spatial import Voronoi, Delaunay
 from scipy.spatial.distance import cdist
 import gudhi
 import itertools as it
 import os
 import collections
 import colorsys
+import igl
+
+
+def write_obj(path, verts, simps, name = "test.001"):
+    dedup_tris = set([tuple(list(i)) for i in simps])
+    dedup_tris = np.array(list(dedup_tris))
+
+    with open(path,'w') as f:
+        f.write("# Blender v2.79 (sub 0) OBJ File: '{}.blend'\n# www.blender.org\no {}\n".format(name, name))
+        for v in verts:
+            f.write('v ' + ' '.join([str(i) for i in v]) + '\n')
+        f.write("s off\n")
+        for s in simps:
+            if len(s) == 2:
+                f.write('l ' + ' '.join([str(i+1) for i in s]) + '\n')
+            else:
+                f.write('f ' + ' '.join([str(i+1) for i in s]) + '\n')
+    pass
 
 def read_obj(path):
     pts = []
@@ -194,10 +212,49 @@ def evolute(pts, thresh, plot=True):
         plt.show()
     return centres
 
+'''
+def write_obj(filename, vertices, faces):
+    with open(filename, 'w') as f:
+        for v in vertices:
+            f.write(f"v {v[0]} {v[1]} {v[2]}\n")
+        for face in faces:
+            f.write(f"f {face[0]} {face[1]} {face[2]}\n")
+'''
+
+def create_mesh_from_arrays(x, y, z):
+    vertices = np.column_stack((x.ravel(), y.ravel(), z.ravel()))
+
+    # Assuming x and y are 2D arrays defining a grid
+    rows, cols = x.shape
+    faces = []
+    for i in range(rows - 1):
+        for j in range(cols - 1):
+            v1 = i * cols + j + 1  # Vertex indices start from 1 in OBJ files
+            v2 = i * cols + j + 2
+            v3 = (i + 1) * cols + j + 1
+            v4 = (i + 1) * cols + j + 2
+            faces.append([v1, v2, v3])
+            faces.append([v2, v4, v3])
+
+    return vertices, faces
+
+def evolute_3d(pts, simps, normals=None):
+    d1, d2, k1, k2 = igl.principal_curvature(pts, simps)
+    if type(normals) == type(None):
+        normals = igl.per_vertex_normals(pts, simps)
+    focal1 = []
+    focal2 = []
+    for p, n, k1i, k2i in zip(pts, normals, k1, k2):
+        focal1.append(p - n/k1i)
+        focal2.append(p - n/k2i)
+    focal1 = np.array(focal1)
+    focal2 = np.array(focal2)
+    return focal1, focal2
+
 # incomplete
 # Estimating Curvatures and Their Derivatives on Triangle Meshes
 # Szymon Rusinkiewicz
-def evolute_3d(pts, v_norms, tris, thresh):
+def my_evolute_3d(pts, v_norms, tris, thresh):
     #calculate coordinate sys for each vertex (u_p, v_p)
     rand_vect = np.random.rand(1,3)
     v_coords = []
@@ -242,7 +299,7 @@ def evolute_3d(pts, v_norms, tris, thresh):
             [x[1], x[2]]])
         #break
         for vert in tri:
-            u_p, v_p = v_coords[vert][0], v_coords[vert][1]
+            u_p, v_p = v_coords[vert]
             e_p = np.matmul(np.matmul(np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]), fund_form2_f), np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]).reshape([2,1]))[0]
             #double check u, v if something goes wrong
             f_p = np.matmul(np.matmul(np.array([np.dot(u_p, u_f.T)[0][0], np.dot(u_p, v_f.T)[0][0]]), fund_form2_f), np.array([np.dot(v_p, u_f.T)[0][0], np.dot(v_p, v_f.T)[0][0]]).reshape([2,1]))[0]
@@ -271,26 +328,46 @@ def compute_2d_lambda_medial_axis(points, lambda_value, farthest=False):
             lambda_ma_points.append(edge)
     lambda_ma_edges = np.array([vor.ridge_dict[tuple(i.tolist())] for i in lambda_ma_points])
 
-    lambda_ma_vertices = lambda_ma_edges[:,0].tolist()
-    lambda_ma_vertices.extend(lambda_ma_edges[:,1].tolist())
-    lambda_ma_vertices = set(lambda_ma_vertices)
-    lambda_ma_vertices = np.array(list(lambda_ma_vertices))
-    lambda_ma_vertices = vertices[lambda_ma_vertices]
-    
+    if len(lambda_ma_edges)>0:
+        lambda_ma_vertices = lambda_ma_edges[:,0].tolist()
+        lambda_ma_vertices.extend(lambda_ma_edges[:,1].tolist())
+        lambda_ma_vertices = set(lambda_ma_vertices)
+        lambda_ma_vertices = np.array(list(lambda_ma_vertices))
+        lambda_ma_vertices = vertices[lambda_ma_vertices]
+    else:
+        lambda_ma_vertices = np.empty([0,3])
     return vertices, lambda_ma_vertices, lambda_ma_edges
 
 # prints the evolute and medial axes for input of multiple curves
-# first curve goes to pts additional curves can be added to args
-def plot_medial_evolute(pts, lambda_value, pt, outfile=None, *args):
-    extra_curves = [ar for ar in args]
+# first curve goes to pts additional (curve, lambda) pairs can be added to args
+def plot_medial_evolute(pts, lambda_value, pt, outfile=None, col0='b', col1='r', *args):
+    extra_curves = [ar[0] for ar in args]
+    extra_lambdas = [ar[1] for ar in args]
     med_points = pts.tolist()
     if len(extra_curves) > 0:
         for curve in extra_curves:
             med_points +=  curve.tolist()
     med_points = np.array(med_points)
-    frac = .2
+
     vertices0, lambda_ma_vertices0, lambda_ma_edges0 = compute_2d_lambda_medial_axis(med_points, lambda_value, False)
     vertices1, lambda_ma_vertices1, lambda_ma_edges1 = compute_2d_lambda_medial_axis(med_points, lambda_value, True)
+    lambda_ma_edges0_all = [lambda_ma_edges0]
+    lambda_ma_edges1_all = [lambda_ma_edges1]
+    vertices0_all = [vertices0]
+    vertices1_all = [vertices1]
+    vertices0, lambda_ma_vertices0, lambda_ma_edges0 = compute_2d_lambda_medial_axis(pts, lambda_value, False)
+    vertices1, lambda_ma_vertices1, lambda_ma_edges1 = compute_2d_lambda_medial_axis(pts, lambda_value, True)
+    lambda_ma_edges0_all += [lambda_ma_edges0]
+    lambda_ma_edges1_all += [lambda_ma_edges1]
+    vertices0_all += [vertices0]
+    vertices1_all += [vertices1]
+    for curve, lambda_i in zip(extra_curves, extra_lambdas):
+        vertices0, lambda_ma_vertices0, lambda_ma_edges0 = compute_2d_lambda_medial_axis(curve, lambda_i, False)
+        vertices1, lambda_ma_vertices1, lambda_ma_edges1 = compute_2d_lambda_medial_axis(curve, lambda_i, True)
+        lambda_ma_edges0_all += [lambda_ma_edges0]
+        lambda_ma_edges1_all += [lambda_ma_edges1]
+        vertices0_all += [vertices0]
+        vertices1_all += [vertices1]
     evos = []
     if len(extra_curves) > 0:
         evos = [evolute(curve, np.inf*lambda_value, plot=False) for curve in extra_curves]
@@ -321,30 +398,42 @@ def plot_medial_evolute(pts, lambda_value, pt, outfile=None, *args):
     #plt.scatter(lambda_ma_vertices[:, 0], lambda_ma_vertices[:, 1], c='g', label='λ-MA Vertices')
     
     # Plot lambda-MA-0 edges
-    for i,edge in enumerate(lambda_ma_edges0):
-        if edge[0] != -1 and edge[1] != -1:  # Avoid infinite edges
-            if i==0:
-                ax1.plot(vertices0[edge][:, 0], vertices0[edge][:, 1], 'g-', label='λ-MA (0)')
-            else:
-                ax1.plot(vertices0[edge][:, 0], vertices0[edge][:, 1], 'g-')
+    i=0
+    for j,(lambda_ma_edges0, vertices0) in enumerate(zip(lambda_ma_edges0_all, vertices0_all)):
+        for edge in lambda_ma_edges0:
+            if edge[0] != -1 and edge[1] != -1:  # Avoid infinite edges
+                if i==0 and j==0:
+                    ax1.plot(vertices0[edge][:, 0], vertices0[edge][:, 1], color=(.5,.5,.5), label='λ-MA (0)')
+                    i=1
+                else:
+                    ax1.plot(vertices0[edge][:, 0], vertices0[edge][:, 1], color=(.5,.5,.5))
     # Plot lambda-MA-1 edges
-    for i,edge in enumerate(lambda_ma_edges1):
-        if edge[0] != -1 and edge[1] != -1:  # Avoid infinite edges
-            if i==0:
-                ax1.plot(vertices1[edge][:, 0], vertices1[edge][:, 1], 'm-', label='λ-MA (1)')
-            else:
-                ax1.plot(vertices1[edge][:, 0], vertices1[edge][:, 1], 'm-')
+    i=0
+    for j,(lambda_ma_edges1, vertices1) in enumerate(zip(lambda_ma_edges1_all, vertices1_all)):
+        for edge in lambda_ma_edges1:
+            if edge[0] != -1 and edge[1] != -1:  # Avoid infinite edges
+                if i==0 and j==0:
+                    ax1.plot(vertices1[edge][:, 0], vertices1[edge][:, 1], color='k', label='λ-MA (1)')
+                    i=1
+                else:
+                    ax1.plot(vertices1[edge][:, 0], vertices1[edge][:, 1], color='k')
     
+
+
     ax1.legend()
     ax1.set_title('2D λ-Medial Axes + Evolute')
     ax1.set_xlabel('X')
     ax1.set_ylabel('Y')
     ax1.axis('equal')
     ax1.grid(True)
-    ax1.legend()
+    ax1.legend(loc = 'lower right')
+    frac = .2
     ax1.set_xlim([pts.T[0].min()*(1 - frac*np.sign(pts.T[0].min())), pts.T[0].max()*(1 + frac*np.sign(pts.T[0].max()))])
     ax1.set_ylim([pts.T[1].min()*(1 - frac*np.sign(pts.T[1].min())), pts.T[1].max()*(1 + frac*np.sign(pts.T[1].max()))])
 
+
+
+    # build extended Persistence diagram
     cplx = gudhi.SimplexTree()
     cplx.set_dimension(2)
     for i in range(len(pts)):
@@ -367,9 +456,9 @@ def plot_medial_evolute(pts, lambda_value, pt, outfile=None, *args):
 
     #gudhi.plot_persistence_diagram(cplx.persistence(), axes=ax2)
     ax2.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
-    ax2.scatter(diag0.T[0], diag0.T[1], color='b', label='0th Diagram')
-    ax2.scatter(diag1.T[0], diag1.T[1], color='r', label='1st Diagram')
-    ax2.legend()
+    ax2.scatter(diag0.T[0], diag0.T[1], color=col0, label='0th Diagram')
+    ax2.scatter(diag1.T[0], diag1.T[1], color=col1, label='1st Diagram')
+    ax2.legend(loc='lower right')
     ax2.set_title('Persistence Diagram')
     ax2.set_xlabel('Birth')
     ax2.set_ylabel('Death')
@@ -381,20 +470,72 @@ def plot_medial_evolute(pts, lambda_value, pt, outfile=None, *args):
     if outfile==None:
          plt.show()
     else:
-        fig.savefig(outfile)
+        fig.savefig(outfile, dpi=180)
     plt.close(fig)  # Close the figure to free up memory
     return dgms
 
+# build extended Persistence diagram
+def plot_extended_persistence3d(pts, simps, pt, outfile=None, col0='b', col1='r', col2='g'):
+    d = max([len(i) for i in simps])
+    cplx = gudhi.SimplexTree()
+    cplx.set_dimension(d)
+    for i, v in enumerate(pts):
+        cplx.insert([i], filtration=np.linalg.norm(v - np.array(pt)))
+    for simp in simps:
+        cplx.insert(simp, filtration=max([cplx.filtration([i]) for i in simp]))
+    #cplx.make_filtration_non_decreasing()
+    cplx.extend_filtration()
+    dgms = cplx.extended_persistence()#persistence_dim_max=2)
+    
+    flat_dgms = [item for sublist in dgms for item in sublist]
+    diag0 = np.array([ i[1] for i in flat_dgms if i[0]==0])
+    diag1 = np.array([ i[1] for i in flat_dgms if i[0]==1])
+    diag2 = np.array([ i[1] for i in flat_dgms if i[0]==2])
+    max_births, max_deaths = max([ i[1][0] for i in flat_dgms]), max([ i[1][1] for i in flat_dgms])
+    max_diag = max(max_births, max_deaths)
+
+    #gudhi.plot_persistence_diagram(cplx.persistence(), axes=ax2)
+    plt.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
+    plt.scatter(diag0.T[0], diag0.T[1], color=col0, label='0th Diagram')
+    plt.scatter(diag1.T[0], diag1.T[1], color=col1, label='1st Diagram')
+    plt.scatter(diag2.T[0], diag2.T[1], color=col2, label='2nd Diagram')
+    plt.legend(loc='lower right')
+    #plt.set_title('Persistence Diagram')
+    #plt.set_xlabel('Birth')
+    #plt.set_ylabel('Death')
+    plt.axis('equal')
+    plt.grid(True)
+    plt.legend()
+    #ax2.set_xlim([-0.1, 15])
+    #ax2.set_ylim([-0.1, 15])
+    if outfile==None:
+        plt.close()  # Close the figure to free up memory
+    else:
+        plt.show()
+    return dgms
 
 
 lambda_val = .4
 n = 1000
 frames = 150
 
+2 \cos \theta
+(1+3/2 \cos^2(\theta/2)) \sin(\theta)
+                
 egg_x = lambda t: ((36 - np.sin(t)*np.sin(t))**(1 / 2) + np.cos(t))*np.cos(t)
 egg_y = lambda t: 4*np.sin(t)
 pts_egg = plot_parametric_2d(egg_x, egg_y, [-np.pi, np.pi], num_points=n, title="2D Parametric Plot")[:-1]
 pts_egg += np.array([0,6])
+
+'''
+2 \cos \theta
+1+3/2 \cos^2(\theta/2)) \sin(\theta)
+'''
+potato_x = lambda t: ((36 - np.sin(t)*np.sin(t))**(1 / 2) + np.cos(t))*np.cos(t)
+potato_y = lambda t: 4*np.sin(t)
+pts_potato = plot_parametric_2d(potato_x, potato_y, [-np.pi, np.pi], num_points=n, title="2D Parametric Plot")[:-1]
+pts_potato += np.array([0,6])
+
 
 ellipse_x = lambda t: 6*np.cos(t)
 ellipse_y = lambda t: 4*np.sin(t)
@@ -404,6 +545,9 @@ pts_ellipse -= np.array([0,6])
 pts = np.array(pts_egg.tolist() + pts_ellipse.tolist())
 
 
+
+'''
+# old animations
 # make animation
 for i,x in enumerate(np.linspace(8, 4, frames)):
     print(i)
@@ -414,42 +558,170 @@ for i,x in enumerate(np.linspace(-1, 3, frames)):
 os.sys("ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence.mp4")
 
 
-
 circ_x = lambda t: np.cos(t)
 circ_y = lambda t: np.sin(t)
 pts_circ = plot_parametric_2d(circ_x, circ_y, [-np.pi, np.pi], num_points=frames, title="2D Parametric Plot")[:-1]
 pts_circ += np.array([1.1, 1.5])
+'''
+
+
+
+############   2d example
+lambda_val = .4
+n = 1000
+frames = 150
+#circ_x = lambda t: (0.1+.05*t/(4*np.pi))*np.cos(t)
+#circ_y = lambda t: (0.1+.05*t/(4*np.pi))*np.sin(t)
+#spiral = plot_parametric_2d(circ_x, circ_y, [0, 4*np.pi], num_points=frames, title="2D Parametric Plot")[:-1]
+#spiral += np.array([-0.174193, 0.162457])
+circ_x = lambda t: 1*np.cos(t)
+circ_y = lambda t: 1*np.sin(t)
+circle = plot_parametric_2d(circ_x, circ_y, [0, 4*np.pi], num_points=frames, title="2D Parametric Plot")[:-1]
+#circle += np.array([-0.174193, 0.162457])
 # make animation
 dgms_all = []
-for i,x in enumerate(pts_circ):
+for i,x in enumerate(circle):
     print(i)
-    dgms_all += [plot_medial_evolute(ellipse_big, 0.1, x, './anim3/frame_{}.png'.format(str(i).zfill(4)), ellipse_small)]
-#os.system("ffmpeg -framerate 30 -pattern_type glob -i './anim3/*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence.mp4")
+    dgms_all += [plot_medial_evolute(wormier, 0.1, x, './anim4/frame_{}.png'.format(str(i).zfill(4)), 'b', 'r')]
+#os.system("ffmpeg -framerate 30 -pattern_type glob -i './anim4/*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence_worm.mp4")
 
 
+
+col0s = [colorsys.hsv_to_rgb(0.25,1,j) for j in np.linspace(0.5,0.9,frames)]
+col1s = [colorsys.hsv_to_rgb(0.75,1,j) for j in np.linspace(0.5,0.9,frames)]
 flat_dgms_all = [[item for sublist in dgm for item in sublist] for dgm in dgms_all]
 max_births, max_deaths = max([max([ i[1][0] for i in flat_dgm]) for flat_dgm in flat_dgms_all]), max([max([ i[1][1] for i in flat_dgm]) for flat_dgm in flat_dgms_all])
 max_diag = max(max_births, max_deaths)
-flat_dgms_all = [[item for sublist in flat_dgms_all for item in sublist] for dgm in flat_dgms_all]
-
-flatter_dgms_all = [item for sublist in flat_dgms_all for item in sublist]
-diag0 = np.array([ i[1] for i in flatter_dgms_all if i[0]==0])
-diag1 = np.array([ i[1] for i in flatter_dgms_all if i[0]==1])
-
-
 plt.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
-plt.scatter(diag0.T[0], diag0.T[1], color='b', label='0th Diagram')
-plt.scatter(diag1.T[0], diag1.T[1], color='r', label='1st Diagram')
-plt.legend()
-#plt.set_title('Persistence Diagram')
-#plt.set_xlabel('Birth')
-#plt.set_ylabel('Death')
+diag0s = [] 
+diag1s = []
+for i,dgm in enumerate(dgms_all):
+    print(i)
+    flat_dgm = [item for sublist in dgm for item in sublist]
+    diag0 = np.array([ i[1] for i in flat_dgm if i[0]==0])
+    diag1 = np.array([ i[1] for i in flat_dgm if i[0]==1])
+    diag0s += np.hstack((diag0, i*1e-1*np.ones([len(diag0),1]))).tolist()
+    diag1s += np.hstack((diag1, i*1e-1*np.ones([len(diag1),1]))).tolist()
+    plt.scatter(diag0.T[0], diag0.T[1], color=col0s[i])
+    plt.scatter(diag1.T[0], diag1.T[1], color=col1s[i])
 plt.axis('equal')
 plt.grid(True)
-plt.legend()
-#plt.set_xlim([-0.1, max_diag])
-#plt.set_ylim([-0.1, max_diag])
 plt.show()
+
+diag0s = np.array(diag0s)
+diag1s = np.array(diag1s)
+
+visited = {}
+alpha=.15
+alpha_cmplx = []
+dela = Delaunay(diag0s)
+for tetra in dela.simplices:
+    edges = it.combinations(tetra,2)
+    for edge in edges:
+        if tuple(sorted(list(edge))) in visited:
+            continue
+        else:
+            verts = np.array(diag0s)[list(edge)]
+            if distance(verts) < alpha:
+                alpha_cmplx.append(tuple(sorted(list(edge))))
+                visited[tuple(sorted(list(edge)))]=1
+alpha_cmplx = np.array(alpha_cmplx)
+
+
+write_obj('vineyard0.obj', diag0s, alpha_cmplx)
+write_obj('vineyard1.obj', diag1s, [])
+
+
+
+
+############   3d example
+pts, simps = read_obj("/home/cfillmor/Downloads/moebius_boundary_2.obj")
+lambda_val = .4
+alpha = 1e-2
+height = 1e-3
+frames = 500
+circ_x = lambda t: 0.125*np.cos(t)
+circ_y = lambda t: 0.125*np.sin(t)
+circle = plot_parametric_2d(circ_x, circ_y, [0, 6*np.pi], num_points=frames, title="2D Parametric Plot")[:-1]
+#circle += np.array([-0.174193, 0.162457])
+# make animation
+dgms_all = []
+for i,y in enumerate(circle):
+    print(i)
+    x = np.array([y[0], y[1], 0])
+    dgms_all += [plot_extended_persistence3d(pts, simps, x, None, 'b', 'r', 'g')]
+#os.system("ffmpeg -framerate 30 -pattern_type glob -i './anim4/*.png' -c:v libx264 -pix_fmt yuv420p extended_persistence_worm.mp4")
+
+col0s = [colorsys.hsv_to_rgb(0.,1,j) for j in np.linspace(0.5,0.9,frames)]
+col1s = [colorsys.hsv_to_rgb(1/3,1,j) for j in np.linspace(0.5,0.9,frames)]
+col2s = [colorsys.hsv_to_rgb(2/3,1,j) for j in np.linspace(0.5,0.9,frames)]
+flat_dgms_all = [[item for sublist in dgm for item in sublist] for dgm in dgms_all]
+max_births, max_deaths = max([max([ i[1][0] for i in flat_dgm]) for flat_dgm in flat_dgms_all]), max([max([ i[1][1] for i in flat_dgm]) for flat_dgm in flat_dgms_all])
+max_diag = max(max_births, max_deaths)
+plt.plot([0, max_diag], [0, max_diag], color='k', label = 'Diagonal')
+diag0s = [] 
+diag1s = []
+diag2s = []
+for i,dgm in enumerate(dgms_all):
+    print(i)
+    flat_dgm = [item for sublist in dgm for item in sublist]
+    diag0 = np.array([ i[1] for i in flat_dgm if i[0]==0])
+    diag1 = np.array([ i[1] for i in flat_dgm if i[0]==1])
+    diag2 = np.array([ i[1] for i in flat_dgm if i[0]==2])
+    diag0s += np.hstack((diag0, i*height*np.ones([len(diag0),1]))).tolist()
+    diag1s += np.hstack((diag1, i*height*np.ones([len(diag1),1]))).tolist()
+    diag2s += np.hstack((diag2, i*height*np.ones([len(diag2),1]))).tolist()
+    plt.scatter(diag0.T[0], diag0.T[1], color=col0s[i])
+    plt.scatter(diag1.T[0], diag1.T[1], color=col1s[i])
+    plt.scatter(diag2.T[0], diag2.T[1], color=col2s[i])
+plt.axis('equal')
+plt.grid(True)
+plt.show()
+
+diag0s = np.array(diag0s)
+diag1s = np.array(diag1s)
+diag2s = np.array(diag2s)
+
+suppress = 0
+visited = {}
+alpha_cmplx = []
+diagXs = diag2s
+dela = Delaunay(diagXs)
+for i, tetra in enumerate(dela.simplices):
+    if int(i*100/len(dela.simplices))%5 == 0:
+        if suppress == 0:
+            print("{}%: ".format(int(i*100/len(dela.simplices))), tetra)
+            suppress = 1
+    else:
+        suppress = 0
+    edges = it.combinations(tetra,2)
+    for edge in edges:
+        if tuple(sorted(list(edge))) in visited:
+            continue
+        else:
+            verts = np.array(diagXs)[list(edge)]
+            if distance(verts) < alpha and np.abs(diagXs[edge[0]][2] - diagXs[edge[1]][2]) > 1e-6 and np.abs(diagXs[edge[0]][2] - diagXs[edge[1]][2]) < height+1e-6:
+                alpha_cmplx.append(tuple(sorted(list(edge))))
+                visited[tuple(sorted(list(edge)))]=1
+alpha_cmplx = np.array(alpha_cmplx)
+
+
+write_obj('vineyard2_3d.obj', diagXs, alpha_cmplx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
